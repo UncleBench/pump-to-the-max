310307a5faa324219ccbb4ca4329d0d2511c1a59
LAG Valentin Buergler <Valentin.Buergler@leuchterag.ch>
2019-09-29 15:22:23 +0200

    Removed an obsolete import

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,7 +2,6 @@ import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { MessagesComponent } from './messages/messages.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
