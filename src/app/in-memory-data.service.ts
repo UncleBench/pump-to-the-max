1b43b333da52d74c2642cba480e77a1b11f8c8b8
LAG Valentin Buergler <Valentin.Buergler@leuchterag.ch>
2019-09-29 17:29:46 +0200

    Added some real exercises to in-memory data service

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -8,16 +8,24 @@ import { Injectable } from '@angular/core';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const exercises = [
-      { id: 11, name: 'Dr Nice' },
-      { id: 12, name: 'Narco' },
-      { id: 13, name: 'Bombasto' },
-      { id: 14, name: 'Celeritas' },
-      { id: 15, name: 'Magneta' },
+      { id: 11, name: 'Bench press' },
+      { id: 12, name: 'Squat' },
+      { id: 13, name: 'Deadlift' },
+      { id: 14, name: 'Dip' },
+      { id: 15, name: 'Leg press' },
       { id: 16, name: 'RubberMan' },
-      { id: 17, name: 'Dynama' },
-      { id: 18, name: 'Dr IQ' },
-      { id: 19, name: 'Magma' },
-      { id: 20, name: 'Tornado' }
+      { id: 17, name: 'Bicep curl' },
+      { id: 18, name: 'Leg extension' },
+      { id: 19, name: 'Leg curl' },
+      { id: 20, name: 'Chest fly' },
+      { id: 21, name: 'Dumbbell bench press' },
+      { id: 22, name: 'Pull down' },
+      { id: 23, name: 'Pull up' },
+      { id: 24, name: 'Bent-over row' },
+      { id: 25, name: 'Military press' },
+      { id: 26, name: 'Shoulder press' },
+      { id: 27, name: 'Lateral raise' },
+      { id: 28, name: 'Tricep extension' },
     ];
     return { exercises };
   }
