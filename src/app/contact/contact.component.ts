19e592d284520d758451aadccaf5ceb359d960c1
LAG Valentin Buergler <Valentin.Buergler@leuchterag.ch>
2019-09-29 16:16:29 +0200

    Fixed some style inconsistencies

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
