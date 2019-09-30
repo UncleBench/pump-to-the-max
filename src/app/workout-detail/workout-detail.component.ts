ba40dd36cfc9fabbf2709c424290ece18b39440e
LAG Valentin Buergler <Valentin.Buergler@leuchterag.ch>
2019-09-30 22:31:05 +0200

    Made WorkoutDetail component work with the new data structure and added edit feature for workouts

--- src/app/workout-detail/workout-detail.component.ts
+++ src/app/workout-detail/workout-detail.component.ts
@@ -3,6 +3,10 @@ import { Workout } from '../workout';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { WorkoutService }  from '../workout.service';
+import { NgSelectOption } from '@angular/forms';
+import { ExerciseService } from '../exercise.service';
+import { Exercise } from '../exercise';
+import { ExerciseUnit } from '../exercise-unit';
 
 @Component({
   selector: 'app-workout-detail',
@@ -12,15 +16,19 @@ import { WorkoutService }  from '../workout.service';
 export class WorkoutDetailComponent implements OnInit {
   
   @Input() workout: Workout;
+  
+  availableExercises: Exercise[];
 
   constructor(
     private route: ActivatedRoute,
     private workoutService: WorkoutService,
+    private exerciseService: ExerciseService,
     private location: Location
   ) { }
 
   ngOnInit() {
     this.getWorkout();
+    this.getExercises();
   }
 
   goBack(): void {
@@ -32,6 +40,12 @@ export class WorkoutDetailComponent implements OnInit {
     this.workoutService
       .getWorkout(id)
       .subscribe(workout => this.workout = workout);
+  }
+
+  getExercises(): void {
+    this.exerciseService
+      .getExercises()
+      .subscribe(availableExercises => this.availableExercises = availableExercises)
   }
 
   save(): void {
